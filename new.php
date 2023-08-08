d4ca635075348cc70e73c16e17b57923b5a49968
tinhvo <tvo229033@gmail.com>
2023-08-08 15:26:00 +0700

    learn PHP

---
+++ new.php
@@ -0,0 +1,108 @@
+<?php
+// $a = "this is text 1";
+// $b = "this is text 2";
+// echo "$a" ." ".  "$b";
+
+#var_dump($a);
+// $MinValue = PHP_INT_MIN;
+// $MaxValue = PHP_INT_MAX; 
+// echo $MinValue."\n";
+// echo $MaxValue; #limit
+
+// $a = 10.19232;
+// var_dump($a); #float
+
+// $x = true;
+// $y = false;
+// echo $x;
+
+
+// $car_array = array("Volvo", "BMW", "Toyota");
+// var_dump($car_array);
+
+// class Car {
+//     public $color;
+//     public $model;
+//     public function __construct($color, $model){
+//         $this->color = $color;
+//         $this->model = $model;
+//     }
+
+//     public function message(){
+//         return "My car is a " . $this->color . " " . $this->model . " !";
+//     }
+
+// }
+
+// $myCar = new Car("red", "Volvo");
+// echo $myCar->message();
+
+// $x = "Hello world";
+// $x = NULL;
+// var_dump($x);
+
+// echo strlen("Hello world") . "\n";
+// echo str_word_count("Hello world") . "\n";
+// echo strrev("Hello world") . "\n";
+// echo strpos("Hello world world", "world") . "\n";
+// echo str_replace("Hello", "Goodbye", "Hello world") . "\n";
+// echo "hello"[0];
+
+
+// $x = 5985;
+// var_dump(is_int($x));
+
+// $x = 59.85;
+// var_dump(is_int($x));
+
+#$x = PHP_FLOAT_DIG;
+#là một hằng số trong PHP, đại diện cho số chữ số chính xác tối đa mà một số floating-point có thể đảm bảo trong hệ thống.
+
+// $x = PHP_FLOAT_EPSILON;
+// echo $x;
+#2.2204460492503E-16
+
+
+//Infinity
+// $x = 1.9e411;
+// var_dump($x);
+
+
+//Not a Number
+// $x = acos(8);
+// var_dump($x);
+
+// $x = 5985;
+// var_dump(is_numeric($x));
+
+// $x = "5985";
+// var_dump(is_numeric($x));
+
+// $x = "59.85" + 100;
+// var_dump(is_numeric($x));
+
+// $x = "Hello";
+// var_dump(is_numeric($x));
+
+
+// $a = 19023.08123;
+// echo (int)$a; 
+
+
+//echo pi();  #3.141592653589793
+
+// echo min(5, 1, 5, 7, 1, 6, 2);
+// echo max(4, 1, 7, 4, 7, 2, 5);
+
+#echo abs(-4.12);
+
+//echo sqrt(64);
+
+// echo round(1.3514);
+// echo round(1.7421);
+// echo round(1.5712);
+
+//echo rand(0, 100);
+
+
+?>
