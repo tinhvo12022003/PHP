a8bd398a994e18019eba04306ec7500e2da35743
tinhvo <tvo229033@gmail.com>
2023-08-09 12:24:57 +0700

    define array, using array() method, index, foreach, for-loop to browse array

---
+++ buoi04.php
@@ -0,0 +1,39 @@
+<?php
+
+#ARRAY
+#using array()
+
+//$cars = array("Volvo", "BMW", "Toyota", "Ford");
+// var_dump($cars);
+
+//echo $cars[0]; //using index
+
+
+// for ($i = 0; $i < count($cars); $i++){ //using for loop 
+//     echo $cars[$i] . " ";
+// }
+
+#PHP Associative Array
+//array using key-value
+//different with array using index
+
+// $person = array("name"=>"Kenus", "age"=>20, "gender"=>"male");
+// var_dump($person);
+// echo "his name is ". $person['name'] . ". He is ". $person['age'] . ". He is " . $person['gender'];
+
+#using foreach loop
+// $person = array("Peter"=>16, "Ben"=>42, "Joe"=>45, "Tom"=>20);
+// foreach($person as $key => $value){ 
+//     echo $key . " is " . $value . "\n";
+// }
+
+#using for loop
+// $key = array_keys($person);
+// $value = array_values($person);
+// for($i=0; $i<count($person); $i++){
+//     echo array_keys($person)[$i] . " is " . array_values($person)[$i] . " year old\n";
+// }
+
+
+
+?>
