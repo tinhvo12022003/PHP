3a49616549aec9fcaa98ab1e7dcf1f61826a6d7f
tinhvo <tvo229033@gmail.com>
2023-08-13 14:54:57 +0700

    open file, read file, write file, close file, feof, fgets methods

---
+++ filehandle.php
@@ -0,0 +1,38 @@
+<?php
+    //$file = fopen("text.txt", "r+") or die("Error opening file");
+    // echo fread($file, 100); //read 100 byte from file
+    // echo fgets($file); //read 1 line from file
+
+    // while(!feof($file)){
+    //     echo fgets($file);
+    //     if(feof($file)){
+    //         echo "\n"."end of file";
+    //         break;
+    //     }
+    // }
+    // fclose($file);
+
+    // while(!feof($file)){
+    //     echo fgetc($file);
+    // }
+    // fclose($file);
+
+
+    // while(($char = fgetc($file)) !== false){
+    //     echo $char;
+    // }
+    // fclose($file);
+    
+
+        // $file = fopen("text.txt", "w") or die("Error opening file");
+        // $text = "hello world!\n1234567890\n";
+        // fwrite($file, $text);
+        // $text = "Japanese\n";
+        // fwrite($file, $text);
+        // fclose($file);
+
+        // $file = fopen("text.txt", 'a');
+        // $text = "PHP";
+        // fwrite($file, $text);
+        // fclose($file);
+?>
